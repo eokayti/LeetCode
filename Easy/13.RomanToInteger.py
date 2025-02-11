983db0f8b4903d94f1d261df23660cd4c28cc540
Lăng Khải Tuấn <106717735+EOKAYTI@users.noreply.github.com>
2025-02-11 22:40:14 +0700

    push

--- Easy/13.RomanToInteger.py
+++ Easy/13.RomanToInteger.py
@@ -1,16 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        symbol =["I","V","X","L","C","D","M"]
-        value = [1,5,10,50,100,500,1000]
-        json = [
-            {"I": 1},
-            {"V": 5},
-            {"X": 10},
-            {"L": 50},
-            {"C": 100},
-            {"D": 500},
-            {"M": 1000}
-        ]
-        for i in range(0, len(symbol)):
-            print(i)
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev_value = 0
+
+        for char in reversed(s):  # Duyệt ngược chuỗi để dễ xử lý quy tắc trừ
+            curr_value = roman_dict[char]
+            if curr_value < prev_value:
+                total -= curr_value
+            else:
+                total += curr_value
+            prev_value = curr_value
         
+        return total
